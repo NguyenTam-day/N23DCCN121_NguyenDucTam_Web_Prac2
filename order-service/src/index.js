bdf28fdf818f2b49e4a0b3ed0f142674025c74d4
Nguyen Tam <nguyenductamxyz@gmail.com>
2026-04-13 23:12:32 +0700

    Fix order service

--- order-service/src/index.js
+++ order-service/src/index.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 });
 
 // ====== START SERVER ======
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
   console.log(`Order Service running on port ${PORT}`);
